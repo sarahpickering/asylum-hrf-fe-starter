acf378c0d83e36d13fac60c16a919ef530a2ca18
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-11-05 09:33:08 -0700

    wrap ProvideAppContext with Auth Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.jsx';
 import { ProvideAppContext } from './context/AppContext.jsx';
+import { Auth0ProviderWithConfig } from './auth/auth0-provider-with-config.js';
+import { BrowserRouter } from 'react-router-dom';
 
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
@@ -14,7 +17,11 @@ const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
  * - Set the domain, clientId, and authorizationParams
  */
 createRoot(document.getElementById('root')).render(
-    <ProvideAppContext>
-      <App />
-    </ProvideAppContext>
+    <BrowserRouter>
+      <Auth0ProviderWithConfig>
+        <ProvideAppContext>
+          <App />
+        </ProvideAppContext>
+      </Auth0ProviderWithConfig>
+    </BrowserRouter>
 );
