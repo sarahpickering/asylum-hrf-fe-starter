9ed43d031f8485e507b162a8ac956cffbd930344
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-11-05 15:58:37 -0700

    implement loginWithDirect and update logout functionality

--- src/auth/LoggingButtons.jsx
+++ src/auth/LoggingButtons.jsx
@@ -1,20 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
-/**
- * TODO: Ticket 3:
- * Implement authentication and logging functionality using Auth0
- */
 export const LoggingButtons = () => {
-  // TODO: Replace these with Auth0 functionality
-  const isAuthenticated = false;
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
 
   const buttonText = isAuthenticated ? 'Log Out' : 'Log In';
 
   const handleLogging = () => {
     if (isAuthenticated) {
-      // TODO: Add Logout functionality here:
+      logout({ returnTo: window.location.origin })
     } else {
-      // TODO: Add Redirect functionality here:
+      loginWithRedirect();
     }
   };
 
