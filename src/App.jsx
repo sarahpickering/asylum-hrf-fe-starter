770738fe4e65a046467e9af937d0f18bdab73f49
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-11-05 15:27:40 -0700

    replace createBrowserRouter with BrowserRouter + Routes + Route and wrapped App w/ Auth0ProviderWithConfig and BrowserRouter in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
 import './App.css';
 import { LandingPage } from './components/pages/Landing';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import { GraphsPage } from './components/pages/DataVisualizations/GraphsPage.jsx';
 import { NotFoundPage } from './components/pages/NotFound/index.jsx';
-import * as React from 'react';
 import Profile from './components/pages/Profile/index.jsx';
 import { pageWrapper } from './components/layout/PageWrapper.jsx';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: pageWrapper(<LandingPage />),
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: '/graphs',
-    element: pageWrapper(<GraphsPage />),
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: '/profile',
-    element: pageWrapper(<Profile />),
-    errorElement: <NotFoundPage />,
-  },
-]);
+const ProtectedProfile = withAuthenticationRequired(Profile, {
+  onRedirecting: () => <div className='text-center p-4'>Redirecting to login...</div>,
+});
 
 export const App = () => {
   return (
     <div className='font-serif w-[100vw] h-[100vh] m-0 flex-c justify-between align-centre text-center min-h-screen secondary-c'>
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path='/' element={pageWrapper(<LandingPage />)} />
+        <Route path='/graphs' element={pageWrapper(<GraphsPage />)} />
+        <Route path='/profile' element={pageWrapper(<ProtectedProfile />)} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 };
